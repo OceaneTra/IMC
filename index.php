cd15be28773d3dad7eed0069055cc5668eff6e97
OceaneTra <oceanetl27@gmail.com>
2024-11-10 23:11:09 +0000

    commit

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ivoire Medical Center - IMC</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link href="node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-- Header -->
+    <nav class="navbar navbar-expand-lg navbar-light p-3 d-flex  ">
+        <a class="navbar-brand text-primary p" href="index.php">Ivoire Medical Center</a>
+        <button class="navbar-toggler  hide" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse align-self-end" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="#">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Présentation</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Tarifs</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Fonctionnalités</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contactez-nous</a></li>
+
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item "><a class="nav-link" href="#">Prendre
+                        rendez-vous</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Connexion</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+    <!-- Presentation Section -->
+    <section class="presentation">
+        <div class="container ">
+            <h1>Application Ivoire Medical Center</h1>
+            <p> Opter pour une application de gestion de centre médical pratique et innovante qui s'adapte à votre
+                activité : Dossier patient informatisé, gestion des rendez-vous, consultations, ordonnances et
+                prescriptions médicales, comptes rendus, facturation et gestion des documents.</p>
+            <div class="buttons">
+                <a href="#" class="btn btn-primary rounded-3">Voir plus</a>
+                <a href="#" class="btn btn-primary">Commencer</a>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- A propos Section -->
+    <section class="container">
+        <div class="row text-center bg-light-subtle ">
+            <p class="my-5 ">
+                IMC est une application permettant une gestion de centre médical efficace, fiable, complètement
+                informatisée,
+                organisée, et simple grâce à son interface interactive, il peut convenir à tout type de médecins
+                généralistes et spécialistes (chirurgien, pneumologue, dermatologue, rhumatologue, neurologue,
+                psychiatre...). Ce logiciel simplifiera votre quotidien grâce à sa grande souplesse d’utilisation.
+            </p>
+        </div>
+    </section>
+
+    <!-- Fonctionnalites Section -->
+
+    <section class="overview-block-ptb grey-bg" id="Fonctionnalité">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="heading-title">
+                        <h3 class="title iq-tw-7"> Fonctionnalités du logiciel</h3>
+                        <p>Notre solution informatique répond à l'ensemble des besoins de gestion de votre cabinet
+                            médicale avec une large souplesse d'utilisation. Son développement a été soigneusement conçu
+                            en tenant compte des différentes préoccupations des médecins</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-lg-3 col-md-6">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-calendar-outline"></i>
+                        </div>
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Gestion de RDV</h5>
+                            <p>Gestion des disponibilités en temps réel lors des prises des rendez-vous avec SMS de
+                                rappel pour vos patients. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 r4-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-people-outline"></i>
+                        </div>
+
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Gestion des Consultations</h5>
+                            <p>Constitution du dossier patient informatisé , historique, suivi des actes effectués.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 r-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-list-outline"></i>
+                        </div>
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Ordonnances et Certificats</h5>
+                            <p>Gestion et impression de vos documents médicaux: certificats, ordonnances, note
+                                d’honoraires</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 r-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-keypad-outline"></i>
+                        </div>
+
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Bilans et Imagerie</h5>
+                            <p>Notre logiciel utilise une base de données des actes biologiques et radiologiques les
+                                plus utilisés dans les établissements de soin.</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 iq-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-copy-outline"></i>
+                        </div>
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Intégration documents</h5>
+                            <p>Le logiciel offre la possibilité d'attacher tout type de documents au dossier patient.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 iq-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-calculator-outline"></i>
+                        </div>
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Facturation et paiement</h5>
+                            <p>Suivi de la facturation de vos prestations et encaissement des recettes.</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 iq-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-ios-email-outline"></i>
+                        </div>
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Gestion de Courrier</h5>
+                            <p>Gestion centralisée de vos correspondances et vos courriers entrants et sortants.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-3 col-md-6 iq-mt-30">
+                    <div class="iq-fancy-box text-center">
+                        <div class="iq-icon">
+                            <i aria-hidden="true" class="ion-pie-graph"></i>
+                        </div>
+                        <div class="fancy-content">
+                            <h5 class="iq-tw-6 iq-pt-20 iq-pb-10">Statistiques détaillées</h5>
+                            <p>Statistiques détaillés de votre activité avec un tableau de bord détaillé.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Forfait Section -->
+    <section class="forfait">
+        <div class="container">
+            <div class="row text-center">
+                <!-- STARTER Plan -->
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <div class="card-header">STARTER</div>
+                        <div class="card-body">
+                            <ul>
+                                <li>En mode Cloud</li>
+                                <li>Gestion des RDV</li>
+                                <li>Gestion des Consultations</li>
+                                <li class="crossed-out">Dossier Patient Informatisé</li>
+                                <li class="crossed-out">Modèles de Prescriptions</li>
+                                <li>Facturation et Recouvrement</li>
+                                <li class="crossed-out">Gestion des Conventions</li>
+                                <li>Tableau de Bord</li>
+                                <li>Sauvegarde Automatique</li>
+                                <li>Support 24/7</li>
+                            </ul> <a href="#" class="btn btn-primary w-100">Commander</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- STARTER Plan -->
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <div class="card-header">STARTER</div>
+                        <div class="card-body">
+                            <ul>
+                                <li>En mode Cloud</li>
+                                <li>Gestion des RDV</li>
+                                <li>Gestion des Consultations</li>
+                                <li class="crossed-out">Dossier Patient Informatisé</li>
+                                <li class="crossed-out">Modèles de Prescriptions</li>
+                                <li>Facturation et Recouvrement</li>
+                                <li class="crossed-out">Gestion des Conventions</li>
+                                <li>Tableau de Bord</li>
+                                <li>Sauvegarde Automatique</li>
+                                <li>Support 24/7</li>
+                            </ul> <a href="#" class="btn btn-primary w-100">Commander</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- STARTER Plan -->
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <div class="card-header">STARTER</div>
+                        <div class="card-body">
+                            <ul>
+                                <li>En mode Cloud</li>
+                                <li>Gestion des RDV</li>
+                                <li>Gestion des Consultations</li>
+                                <li class="crossed-out">Dossier Patient Informatisé</li>
+                                <li class="crossed-out">Modèles de Prescriptions</li>
+                                <li>Facturation et Recouvrement</li>
+                                <li class="crossed-out">Gestion des Conventions</li>
+                                <li>Tableau de Bord</li>
+                                <li>Sauvegarde Automatique</li>
+                                <li>Support 24/7</li>
+                            </ul> <a href="#" class="btn btn-primary w-100">Commander</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Avis Section -->
+    <section class="blog">
+
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact flex-column d-flex pb-5">
+        <div>
+
+        </div>
+        <h1 class=" mb-4 text-center ">Contactez-nous</h1>
+        <form id="contact-form" method="post" action="" role="form" class="mx-5 p-5 w-50 align-self-center">
+
+            <div class="messages"></div>
+
+
+            <div class="controls">
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="nom">Nom *</label>
+                            <input id="nom" type="text" name="nom" class="form-control" placeholder="Entrer votre nom *"
+                                required="required" data-error="Votre nom est requis.">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="prenom">Prénom *</label>
+                            <input id="prenom" type="text" name="prenom" class="form-control"
+                                placeholder="Entrer votre prénom *" required="required"
+                                data-error="Votre prénom est requis.">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="email">Email *</label>
+                            <input id="email" type="email" name="email" class="form-control"
+                                placeholder="Please enter your email *" required="required"
+                                data-error="Valid email is required.">
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="form_need">Objet *</label>
+                            <select id="form_need" name="need" class="form-control" required="required"
+                                data-error="Précisez l'objet de votre message.">
+                                <option value=""></option>
+                                <option value="Request quotation">Request quotation</option>
+                                <option value="Request order status">Request order status</option>
+                                <option value="Request copy of an invoice">Request copy of an invoice</option>
+                                <option value="Other">Other</option>
+                            </select>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="form_message">Message *</label>
+                            <textarea id="form_message" name="message" class="form-control"
+                                placeholder="Message for me *" rows="4" required="required"
+                                data-error="Please, leave us a message."></textarea>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-4">
+                        <input type="submit" class="btn btn-success btn-send" value="Send message">
+                    </div>
+                </div>
+
+            </div>
+
+        </form>
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row align-items-center d-flex justify-content-between">
+                <!-- Logo and Description -->
+                <div class="col-md-3 ">
+                    <div class="logo ">Ivoire Medical Center</div>
+                    <p class="description text-sm mt-3">Notre solution informatique répond à l'ensemble des besoins de
+                        gestion de
+                        votre centre médical avec une large souplesse d'utilisation.</p>
+                </div>
+                <div class="col-6">
+                    <div class="row row-gap-sm-2">
+                        <!-- Contact Information -->
+                        <div class="col">
+                            <h5>Contactez-nous</h5>
+                            <p>Email: <a href="mailto:hello@dreamy.co">contact@imc.co</a></p>
+                            <p>Entreprise: IMC Côte d'Ivoire</p>
+                            <p>Tel: +225 07 59 39 58 41</p>
+                        </div>
+                        <!-- Social Media Links -->
+                        <div class="col">
+                            <h5>Réseaux sociaux</h5>
+                            <p>Oui, nous sommes sur les réseaux sociaux.</p>
+                            <div class="social align-items-center">
+                                <a href="#"><i class=" fa-facebook"></i></a>
+                                <a href="#"><i class=" fa-twitter"></i></a>
+                                <a href="#"><i class=" fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-12 mt-3 border-top copyright"> &copy; 2024 Equipe Ivoire Medical
+                    Center. Tous
+                    droits
+                    réservés. </div>
+            </div>
+        </div>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+
+</body>
+
+</html>
